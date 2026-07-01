0722b3024747195d589dca862f778f198fea3b5b
Manoj Boganadham <54001355+manoj-boganadham@users.noreply.github.com>
2026-07-01 07:42:01 +0530

    Update event dates and share message in invitationData

--- src/data/invitationData.js
+++ src/data/invitationData.js
@@ -38,16 +38,24 @@ const invitationData = {
     {
       id: 'haldi ',
       title: 'Haldi and Pellikoduku ',
-      date: 'Sunday, July 01, 2026',
+      date: 'Wednesday, July 01, 2026',
       time: '07:00 AM',
       venue: `Groom's Residence - Radhakrishna General Stores (Madhavaram-1, Kadapa)`,
       mapsQuery: 'https://www.google.com/maps/place/%E0%B0%B0%E0%B0%BE%E0%B0%A7%E0%B0%BE%E0%B0%95%E0%B1%83%E0%B0%B7%E0%B1%8D%E0%B0%A3+%E0%B0%9C%E0%B0%A8%E0%B0%B0%E0%B0%B2%E0%B1%8D+%E0%B0%B8%E0%B1%8D%E0%B0%9F%E0%B1%8B%E0%B0%B0%E0%B1%8D%E0%B0%B8%E0%B1%8D/@14.4116225,78.977096,17z/data=!3m1!4b1!4m6!3m5!1s0x3bb36ad9ff4c5393:0xf918995f3d80342b!8m2!3d14.4116225!4d78.9796709!16s%2Fg%2F11xtx8vzjh!18m1!1e1?entry=ttu&g_ep=EgoyMDI2MDYxNi4wIKXMDSoASAFQAw%3D%3D',
+    },    
+    {
+      id: 'haldi ',
+      title: 'Haldi and Pellikoothuru ',
+      date: 'Wednesday, July 01, 2026',
+      time: '07:00 AM',
+      venue: `Bride's Residence - SRNB Residency`,
+      mapsQuery: 'https://maps.app.goo.gl/GXCxRRsb9PAfqDSt8?g_st=iw',
     },
     {
       id: 'reception',
       title: 'Reception, Orchestra & Dinner',
       subtitle: 'Celebration Evening',
-      date: 'Sunday, July 02, 2026',
+      date: 'Thursday, July 02, 2026',
       time: '06:00 PM',
       venue: 'NMS Kalyanamandapam, Mydukur Road, Badvel',
       mapsQuery: 'NMS Kalyanamandapam, Mydukur Road, Badvel',
@@ -64,7 +72,7 @@ const invitationData = {
   ],
   share: {
     whatsappText:
-      'You are invited to celebrate the wedding of Tharuni & Rahul. We would love to have you with us.',
+      'Your presence would mean the world to us as we begin our journey together ❤️',
   },
   footerMessage: 'We look forward to celebrating with you',
 };
